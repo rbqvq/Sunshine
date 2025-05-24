a0abe4b131b234b13a8a9b884572bb34f38d42d2
Coia Prant <coiaprant@gmail.com>
2025-05-25 01:35:06 +0800

    fix(build): replace boost to windows arm64 patched
    Backported latest boost context `CMakeLists.txt` to boost v1.87.0
    
    That supports Windows Arm64
    
    Signed-off-by: Coia Prant <coiaprant@gmail.com>

--- cmake/dependencies/Boost_Sunshine.cmake
+++ cmake/dependencies/Boost_Sunshine.cmake
@@ -55,7 +55,7 @@ if(NOT Boost_FOUND)
     # Limit boost to the required libraries only
     set(BOOST_INCLUDE_LIBRARIES ${BOOST_COMPONENTS})
     set(BOOST_URL "https://github.com/boostorg/boost/releases/download/boost-${BOOST_VERSION}/boost-${BOOST_VERSION}-cmake.tar.xz")  # cmake-lint: disable=C0301
-    set(BOOST_HASH "SHA256=7da75f171837577a52bbf217e17f8ea576c7c246e4594d617bfde7fafd408be5")
+    set(BOOST_URL "https://github.com/rbqvq/Sunshine/releases/download/boost-${BOOST_VERSION}/boost-${BOOST_VERSION}-cmake.tar.xz")  # cmake-lint: disable=C0301
 
     if(CMAKE_VERSION VERSION_LESS "3.24.0")
         FetchContent_Declare(
